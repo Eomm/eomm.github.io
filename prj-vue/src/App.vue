4feb751140a698a06ef1abbcfaac685b30024b95
Manuel Spigolon <behemoth89@gmail.com>
2019-05-11 12:48:49 +0200

    vue exp

---
+++ prj-vue/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <stat  type="Disciplina" max="3" />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Stat from './components/Stat.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld,
+    Stat
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ prj-vue/src/components/Stat.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="stat">
+    <p>{{ type }} {{current}}</p>
+    <ul v-for="c in dice" v-bind:key="c.id">
+      <li>
+        <input type="checkbox" @change="current+=1">
+        {{c.id}}
+      </li>
+    </ul>
+    fine
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'stat',
+  props: {
+    type: String,
+    max: Number,
+    active: Number
+  },
+  data: function (params) {
+    return {
+      dice: []
+    }
+  },
+  created: function (params) {
+    let startActive = this.active
+    for (let i = 0; i < this.max; i++) {
+      this.dice.push({ id: i, active: (startActive--) > 0 })
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
